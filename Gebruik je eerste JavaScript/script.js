ea44f9a2d505d23c37a3080277cad7395e502bd9
BA Dijkman <85498996+BADijkman@users.noreply.github.com>
2021-07-06 16:17:01 +0200

    Add files via upload

---
+++ Gebruik je eerste JavaScript/script.js
@@ -0,0 +1,25 @@
+let naam = "Andre";
+let leeftijd = 52;
+
+ console.log(naam);
+ console.log(leeftijd);
+
+ console.log(4+4);
+ console.log('4' + '4');
+
+ console.log (9+6);
+ console.log (9-6);
+ console.log (9*6);
+ console.log (9/3);
+ console.log (9%4);
+
+
+ let numberOne = 4;
+ let numberTwo = 4;
+ console.log( numberOne + numberTwo);
+
+ console.log(typeof naam);
+ console.log(typeof leeftijd);
+
+ leeftijd = "52";
+ console.log(typeof leeftijd);

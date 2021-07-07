f1910f768ab602f66d2f4db56458755230471eb2
BA Dijkman <85498996+BADijkman@users.noreply.github.com>
2021-07-07 13:07:51 +0200

    Add files via upload

---
+++ _level12_javascript+comments/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Winc Academy studenten");
+
+let getalEen = 1230941;
+let getalTwee = 1823794;
+// Dit is een grote  som
+console.log(getalEen * getalTwee);
+
+/* console.log(1230941 *1823794 );
+
+getalEen= 637263;
+getalTwee=54;
+console.log( getalEen / getalTwee);
+console.log( 637263 / 54);
+ */
+

fd4182f340de86cd8c559eaa240dd1e1fe96ddf2
BA Dijkman <85498996+BADijkman@users.noreply.github.com>
2021-07-08 01:59:56 +0200

    Update ifelse.js

--- level13/if-else/ifelse.js
+++ level13/if-else/ifelse.js
@@ -3,29 +3,29 @@ const isFemale = true;
 const driverStatus = "bob";
 
 if (age >= 25){
-    console.log("Qua leeftijd mag deze persoon naar binnen.")
+    console.log("Qua leeftijd mag deze persoon naar binnen.");
 }else{
-    console.log("Qua leeftijd mag deze persoon NIET naar binnen.")
+    console.log("Qua leeftijd mag deze persoon NIET naar binnen.");
 }
 
 if (isFemale){
-    console.log("Deze persoon is een vrouw.")
+    console.log("Deze persoon is een vrouw.");
     }
     else{
-        console.log("Deze persoon is geen vrouw.")
+        console.log("Deze persoon is geen vrouw.");
 }
 
 if (driverStatus === "bob"){
-    console.log("Deze persoon heeft de status van bob en mag rijden.")
+    console.log("Deze persoon heeft de status van bob en mag rijden.");
     }
     else{
-        console.log("Deze persoon heeft NIET de status van bob en mag NIET rijden.")
+        console.log("Deze persoon heeft NIET de status van bob en mag NIET rijden.");
 } 
     
 if(age >= 25 &&  isFemale && driverStatus == "bob"){
-    console.log("Deze persoon mag naar binnen.")
+    console.log("Deze persoon mag naar binnen.");
 } else{
-    console.log("Deze persoon mag NIET naar binnen.")
+    console.log("Deze persoon mag NIET naar binnen.");
 }
 
 

328c6eae7772cc89dae21814f7c6de6fc778c1b4
BA Dijkman <85498996+BADijkman@users.noreply.github.com>
2021-07-09 13:43:07 +0200

    Update functions.js

--- level_14/function_basic/functions.js
+++ level_14/function_basic/functions.js
@@ -1,9 +1,8 @@
-let makePizza = function(){
+  let makePizza = function(){
     console.log("maak het pizza deeg");
     console.log("verwarm de oven");
     console.log("plaats pizza in oven");
   }
-  
   let makeSushi = function(){
     console.log("kook Sushirijst");
     console.log("meng de Sushirijst met de sushi azijn");

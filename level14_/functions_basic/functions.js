1281760d620a6f10b98ea1453f048f73ea3b67b0
BA Dijkman <85498996+BADijkman@users.noreply.github.com>
2021-07-09 13:41:18 +0200

    Add files via upload

---
+++ level14_/functions_basic/functions.js
@@ -0,0 +1,17 @@
+let makePizza = function(){
+    console.log("maak het pizza deeg");
+    console.log("verwarm de oven");
+    console.log("plaats pizza in oven");
+  }
+  
+  let makeSushi = function(){
+    console.log("kook Sushirijst");
+    console.log("meng de Sushirijst met de sushi azijn");
+    console.log("De sushi rollen");
+  }
+  
+  makeSushi();
+  makeSushi();
+  makeSushi();
+  makePizza();
+  makePizza();
